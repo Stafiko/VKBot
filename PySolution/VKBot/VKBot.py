aaf9ae3d894fa8796205e63c5dcd1cf16461d0b7
Egor Mukhachev <egor.ork@mail.ru>
2017-05-11 21:00:58 +0300

    Changed env. to Py3.6, optimized code for this env. Added answer (last:netu) Fixed whitelist (now only ints)

--- PySolution/VKBot/VKBot.py
+++ PySolution/VKBot/VKBot.py
@@ -1,33 +1,35 @@
 # -*- coding: utf-8 -*-
 import vk_api
-import string
 import re
+import string
 from itertools import *
 from vk_api.longpoll import VkLongPoll, VkEventType
 
 login, password = '89870195625', '258262St'
 vk = vk_api.VkApi(login, password)
-whitelist = (25069332,'25069332')
+whitelist = (25069332,)
+
 class answer:
     def __init__(self, d, a):
         self.dict = d
         self.ans = a
 
-ans_for_end = [ answer((u'тристо',u'триста',u'трицто',u'трицта',
-	                    u'300',u'3оо',u'3о0',u'30о',
-		                u'3сто',u'3ста',u'3цто',u'3цта',
-		                u'три100',u'три1оо',u'три1о0',u'три10о'),u'отсоси у программиста'),
-                answer((u'ет',),u'пидора ответ'),
-                answer((u'да',),u'на хую борода')]
+ans_for_end = [ answer(('тристо','триста','трицто','трицта',
+	                    '300','3оо','3о0','30о',
+		                '3сто','3ста','3цто','3цта',
+		                'три100','три1оо','три1о0','три10о'),'отсоси у программиста'),
+                answer(('ет',),'пидора ответ'),
+                answer(('да',),'на хую борода')]
 
-ans_for_last = [answer((u'куда',u'кида'),u'туда, куда не ходят поезда'),
-                answer((u'тоже',u'тоге',u'тойе'),u'на говно похоже'),
-                answer((u'где',),u'в караганде'),
-                answer((u'ок',),u'пидора кусок'),
-                answer((u'я',u'йа',u'иа'),u'головка от хуя'),
-                answer((u'кек',),u'лол арбидол'),
-                answer((u'лол',),u'кек чебурек'),
-                answer((u'че',u'чо'),u'хуй через плечо')]
+ans_for_last = [answer(('куда','кида'),'туда, куда не ходят поезда'),
+                answer(('тоже','тоге','тойе'),'на говно похоже'),
+                answer(('где',),'в караганде'),
+                answer(('ок',),'пидора кусок'),
+                answer(('я','йа','иа'),'головка от хуя'),
+                answer(('кек',),'лол арбидол'),
+                answer(('лол',),'кек чебурек'),
+                answer(('че','чо'),'хуй через плечо'),
+                answer(('нету','нети'),'нет слова "нету", безграмотный')]
 
 def del_repeats(match):
 	return match.group(0)[0]
@@ -38,29 +40,24 @@ def write_chat(id, mes):
 def write_user(id, mes):
     vk.method('messages.send', {'user_id':id,'message':mes})
 
-def maketrans_unicode(s1, s2, todel=""):
-	trans_tab = dict( zip( map(ord, s1), map(ord, s2) ) )
-	trans_tab.update( (ord(c),None) for c in todel )
-	return trans_tab
-	
 def answer_mes(message, user_id, user_name, from_chat):
-    if user_id in whitelist: return 'nope'
+    if int(user_id) in whitelist: return 'nope'
     if from_chat: preff = '@id'+str(user_id)+' ('+user_name+'), '
     else: preff = ''
-    delimetrs = maketrans_unicode(
-        u'qwertyuiopasdfghjklzxcvbnm',
-        u'квертиуиопасдфгхйклзхцвбнм',
-        string.punctuation+u'ьъ'+"!@#$%^&amp;*()-_=+{}[];:'&quot;&lt;&gt;,./?|\\")
+    delimetrs = str.maketrans(
+        'qwertyuiopasdfghjklzxcvbnm',
+        'квертиуиопасдфгхйклзхцвбнм',
+        string.punctuation+'ьъ'+"!@#$%^&amp;*()-_=+{}[];:'&quot;&lt;&gt;,./?|\\")
     message = message.lower().translate(delimetrs)
     
     #300
     if message.endswith(tuple(ans_for_end[0].dict)): return preff+ans_for_end[0].ans
 
-    message = message.translate(maketrans_unicode('','',string.digits))
+    message = message.translate(str.maketrans('','',string.digits))
     message = re.sub(r'(.)\1+', del_repeats, message)
     last_word = message.split(' ')[-1]
     message.replace(' ','')
-    for e,l in izip_longest(ans_for_end,ans_for_last):
+    for e,l in zip_longest(ans_for_end,ans_for_last):
         if e != None and message.endswith(tuple(e.dict)): return preff+e.ans
         elif l != None and last_word in l.dict: return preff+l.ans
     return 'nope'
@@ -76,39 +73,39 @@ def main():
     for event in longpoll.listen():
 
         if event.type == VkEventType.MESSAGE_NEW:
-            print(u'Новое сообщение: ')
+            print('Новое сообщение: ')
             user_name = vk.method('users.get', {'user_ids':event.user_id})[0]['first_name']
             mes = answer_mes(event.text, event.user_id, user_name, event.from_chat)
-            print(u'От кого: ' + user_name)
-            print(u'Ответочка: ' + mes)
-            if event.from_me: print(u'От меня для: ')
-            elif event.to_me: print(u'Для меня от: ')
+            print('От кого:',user_name)
+            print('Ответочка:',mes)
+            if event.from_me: print(u'От меня для:')
+            elif event.to_me: print(u'Для меня от:')
 
             if event.from_user and event.to_me:
                 if mes != 'nope': write_user(event.user_id, mes)
                 print(event.user_id)
             elif event.from_chat and event.to_me:
                 if mes != 'nope': write_chat(event.chat_id, mes)
-                print(event.user_id, u' в беседе ', event.chat_id)
+                print(event.user_id, 'в беседе', event.chat_id)
             elif event.from_group:
-                print(u' группы ', event.group_id)
+                print('группы', event.group_id)
 
-            print(u'Текст: ',event.text)
+            print('Текст:',event.text)
 
         elif event.type == VkEventType.USER_TYPING:
-            print(u'Печатает ')
+            print('Печатает ')
 
             if event.from_user: print(event.user_id)
-            elif event.from_group: print(u' администратор группы ', event.group_id)
+            elif event.from_group: print('администратор группы', event.group_id)
 
         elif event.type == VkEventType.USER_TYPING_IN_CHAT:
-            print(u'Печатает ', event.user_id, u' в беседе ', event.chat_id)
+            print('Печатает', event.user_id, 'в беседе', event.chat_id)
 
         elif event.type == VkEventType.USER_ONLINE:
-            print(u'Пользователь', event.user_id, u' онлайн ', event.platform)
+            print('Пользователь', event.user_id, 'онлайн', event.platform)
 
         elif event.type == VkEventType.USER_OFFLINE:
-            print(u'Пользователь', event.user_id, u' оффлайн ', event.offline_type)
+            print('Пользователь', event.user_id, 'оффлайн', event.offline_type)
 
         else:
             print(event.type, event.raw[1:])
